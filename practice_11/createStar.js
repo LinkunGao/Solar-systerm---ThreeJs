1bbc84305b961ec0fab08f6ec7d79e2d0c0d1da4
LinkunGao <gaolinkun123@gmail.com>
2021-11-30 22:31:03 +1300

    add uv animation to sun

---
+++ practice_11/createStar.js
@@ -0,0 +1,94 @@
+import * as THREE from "../three/build/three.module.js";
+
+const textloader = new THREE.TextureLoader();
+
+function createMesh(geometry, URL) {
+  let text_map = textloader.load(URL);
+  const material = new THREE.MeshBasicMaterial({
+    map: text_map,
+    side: THREE.DoubleSide,
+    transparent: true, // 开启透明模式
+  });
+
+  let mesh_sphere = new THREE.Mesh(geometry, material);
+  return { mesh_sphere, text_map };
+}
+
+function createNormalMesh(geometry, URL_map, URL_nor) {
+  const material = new THREE.MeshPhongMaterial({
+    map: textloader.load(URL_map),
+    normalMap: textloader.load(URL_nor),
+    normalScale: new THREE.Vector2(4, 4),
+    side: THREE.DoubleSide,
+    transparent: true, // 开启透明模式
+  });
+
+  return new THREE.Mesh(geometry, material);
+}
+// para: radius
+// 提高球体的复用率
+const geometry = new THREE.SphereGeometry(1, 100, 100);
+function createSphereMesh(name, R, URL) {
+  let { mesh_sphere, text_map } = createMesh(geometry, URL);
+
+  mesh_sphere.scale.set(R, R, R);
+  mesh_sphere.name = name;
+  if (name === "Sun") {
+    return { mesh_sphere, text_map };
+  }
+  return mesh_sphere;
+}
+
+function createNorSphereMesh(name, R, URL_map, URL_nor) {
+  // const geometry = new THREE.SphereGeometry(R, 100, 100);
+  const mesh_nor_sphere = createNormalMesh(geometry, URL_map, URL_nor);
+  mesh_nor_sphere.scale.set(R, R, R);
+  mesh_nor_sphere.name = name;
+  return mesh_nor_sphere;
+}
+
+function createRingMesh(inner_r, outer_R, URL) {
+  const geometry_ring = new THREE.RingGeometry(inner_r, outer_R, 32);
+  const { mesh_sphere, text_map } = createMesh(geometry_ring, URL);
+
+  return mesh_sphere;
+}
+
+function createPlanetMesh(
+  name,
+  sphere_R,
+  shpere_url,
+  inner_r,
+  outer_R,
+  ring_URL
+) {
+  const group = new THREE.Group();
+  const shpere = createSphereMesh(name, sphere_R, shpere_url);
+  const ring = createRingMesh(inner_r, outer_R, ring_URL);
+  //   调整姿态
+  ring.rotateX(Math.PI / 2);
+  return group.add(shpere, ring);
+}
+
+// use ArcCurve to draw a curve
+// Math.PI==180
+const arc = new THREE.ArcCurve(0, 0, 1, 0, Math.PI * 2, true);
+// the more points the curve more smooth
+const points = arc.getPoints(300); // it will reture a vector2 array
+// new THREE.Geometry()不能使用了，现在用new THREE.BufferGeometry()来代替
+const geometry_line = new THREE.BufferGeometry();
+//setFromPoints方法的本质：遍历points把vector2转变化vector3
+geometry_line.setFromPoints(points);
+const material_line = new THREE.LineBasicMaterial({
+  color: 0x006666,
+});
+
+function circle(r) {
+  const line = new THREE.LineLoop(geometry_line, material_line);
+  line.scale.set(r, r, r);
+  // 圆弧线默认在XOY平面上，绕x轴旋转到XOZ平面上
+  line.rotateX(Math.PI / 2);
+  return line;
+}
+
+export { createSphereMesh, createNorSphereMesh, createPlanetMesh, circle };
---
+++ practice_11/main.js
@@ -0,0 +1,289 @@
+import * as THREE from "../three/build/three.module.js";
+import { OrbitControls } from "../three/examples/jsm/controls/OrbitControls.js";
+import * as CREATE from "./createStar.js";
+import { getData, getPlanetData } from "./data.js";
+import * as C_Tag from "./addTag.js";
+
+// all stars data
+const data = getData();
+const planetData = getPlanetData();
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+  antialias: true,
+});
+const scene = new THREE.Scene();
+
+// camera;
+// const fov = 75;
+// const aspect = window.innerWidth / window.innerHeight;
+// const near = 1;
+// const far = 1500;
+
+// const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+// 正交投影相机
+const aspect = window.innerWidth / window.innerHeight; //窗口宽高比
+//可以根据最外围的海王星公转半径100 * 5设置改参数
+const s = 620; //s参数影响相机渲染的上下左右范围
+//创建相机对象
+// 注意相机参数6远裁界面可以包含全部星体在内
+const camera = new THREE.OrthographicCamera(
+  -s * aspect,
+  s * aspect,
+  s,
+  -s,
+  1,
+  1500
+);
+
+camera.position.set(500, 613, 525); //设置相机位置
+camera.lookAt(scene.position); //设置相机方向(指向的场景对象)
+
+// control
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
+controls.update();
+
+// geometry sun
+// public
+// 公转思路一
+const solar_system = [];
+// 公转思路二
+// const planetGroup = new THREE.Group();
+const sunOrib = new THREE.Object3D();
+scene.add(sunOrib);
+// solar_system.push(sunOrib);
+
+const { mesh_sphere, text_map } = CREATE.createSphereMesh(
+  data.sun.name,
+  data.sun.R,
+  data.sun.URL
+);
+// console.log(CREATE.createSphereMesh(data.sun.name, data.sun.R, data.sun.URL));
+// console.log(sun, sun_map);
+const sun = mesh_sphere;
+text_map.wrapS = THREE.RepeatWrapping;
+text_map.wrapT = THREE.RepeatWrapping;
+sunOrib.add(sun);
+sun.tag = C_Tag.createTag(sun.name);
+// planetGroup.add(sun);
+
+solar_system.push(sun);
+data.planet.forEach((planet) => {
+  const line = CREATE.circle(planet.revolutionR);
+  scene.add(line);
+  // 计算角度，让每个星球的分布不同
+  let angle = 2 * Math.PI * Math.random();
+  if (planet.name === "Earth") {
+    const Earth_Orib = new THREE.Object3D();
+    const earth = CREATE.createNorSphereMesh(
+      planet.name,
+      planet.R,
+      planet.URL,
+      planet.URL_Nor
+    );
+    const Moon_Orib = new THREE.Object3D();
+    const moon = CREATE.createNorSphereMesh(
+      planet.moon.name,
+      planet.moon.R,
+      planet.moon.URL,
+      planet.moon.URL_Nor
+    );
+
+    Earth_Orib.R = planet.R;
+    Earth_Orib.tag = C_Tag.createTag(earth.name);
+    Moon_Orib.R = planet.moon.R;
+    Moon_Orib.tag = C_Tag.createTag(moon.name);
+
+    Earth_Orib.name = "earth";
+    Earth_Orib.angle = angle;
+    Earth_Orib.revolutionR = planet.revolutionR;
+    Earth_Orib.position.x = planet.revolutionR;
+    Moon_Orib.position.x = 35;
+    Moon_Orib.angle = angle;
+    Moon_Orib.name = "moon";
+    Moon_Orib.revolutionR = 35;
+
+    sunOrib.add(Earth_Orib);
+    Earth_Orib.add(earth);
+    Earth_Orib.add(Moon_Orib);
+    Moon_Orib.add(moon);
+
+    solar_system.push(earth);
+    solar_system.push(moon);
+  } else if (planet.name === "Saturn" || planet.name === "Uranus") {
+    const star_Orib = new THREE.Object3D();
+    const star = CREATE.createPlanetMesh(
+      planet.name,
+      planet.shpere.R,
+      planet.shpere.URL,
+      planet.ring.r,
+      planet.ring.R,
+      planet.ring.URL
+    );
+
+    star_Orib.name = star.children[0].name;
+    // star_Orib.position.z = planet.recolutionR;
+    star_Orib.position.set(
+      planet.revolutionR * Math.sin(angle),
+      0,
+      planet.revolutionR * Math.cos(angle)
+    );
+    star_Orib.tag = C_Tag.createTag(star.children[0].name);
+    star_Orib.angle = angle;
+    star_Orib.revolutionR = planet.revolutionR;
+    star_Orib.add(star);
+    sunOrib.add(star_Orib);
+    solar_system.push(star_Orib);
+  } else {
+    const Orib = new THREE.Object3D();
+
+    const star = CREATE.createSphereMesh(planet.name, planet.R, planet.URL);
+
+    const x = planet.revolutionR * Math.sin(angle);
+    const z = planet.revolutionR * Math.cos(angle);
+
+    Orib.name = star.name;
+    Orib.tag = C_Tag.createTag(star.name);
+    Orib.position.set(x, 0, z);
+    Orib.angle = angle;
+    Orib.revolutionR = planet.revolutionR;
+    Orib.add(star);
+    sunOrib.add(Orib);
+    solar_system.push(Orib);
+  }
+});
+
+// 坐标轴用于辅助开发  x、y、z轴颜色分别为R、G、B
+// const axesHelper = new THREE.AxesHelper(200);
+// scene.add(axesHelper);
+
+// light
+const directionLight_F = new THREE.DirectionalLight(0xffffff, 0.9);
+directionLight_F.position.set(400, 200, 300);
+scene.add(directionLight_F);
+
+const directionLight_B = new THREE.DirectionalLight(0xffffff, 0.9);
+directionLight_B.position.set(-400, -200, -300);
+scene.add(directionLight_B);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+function resizeRenderTosiplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const pixelRatio = window.devicePixelRatio;
+  const width = (canvas.clientWidth * pixelRatio) | 0;
+  const height = (canvas.clientHeight * pixelRatio) | 0;
+
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    // false is very very important here
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+// solar_system[0].children.forEach((a) => {
+//   console.log(a.revolutionR);
+// });
+
+let selectMesh = null;
+let planet_div = C_Tag.createImg();
+
+function render() {
+  if (resizeRenderTosiplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+
+  renderer.render(scene, camera);
+
+  C_Tag.tagXYVertex(sun, camera);
+
+  // 让太阳流动
+  text_map.offset.x -= 0.001;
+  text_map.offset.y += 0.001;
+
+  // solar_system[0].children.forEach((planet) => {
+  sunOrib.children.forEach((planet) => {
+    planet.rotateY(0.02);
+    if (planet.angle) {
+      if (planet.name == "earth") {
+        planet.rotateY(0.01);
+        planet.children.forEach((star) => {
+          if (star.name === "moon") {
+            C_Tag.tagXYVertex(star, camera);
+            // console.log(star.revolutionR);
+            star.angle += (0.005 / star.revolutionR) * 130;
+            star.position.set(
+              star.revolutionR * Math.sin(star.angle),
+              0,
+              star.revolutionR * Math.cos(star.angle)
+            );
+            // star.position.set(15, 0, 15);
+          }
+        });
+      }
+      // 半径越大，转动速度越慢
+      planet.angle += (0.005 / planet.revolutionR) * 300;
+      // 行星公转过程位置设置
+      planet.position.set(
+        planet.revolutionR * Math.sin(planet.angle),
+        0,
+        planet.revolutionR * Math.cos(planet.angle)
+      );
+      C_Tag.tagXYVertex(planet, camera);
+    }
+  });
+  requestAnimationFrame(render);
+
+  if (selectMesh) {
+    const worldVetor = new THREE.Vector3();
+    selectMesh.getWorldPosition(worldVetor);
+    const standardVertor = worldVetor.project(camera);
+    const a = window.innerWidth / 2;
+    const b = window.innerHeight / 2;
+    const x = Math.round(standardVertor.x * a + a);
+    const y = Math.round(-standardVertor.y * b + b);
+
+    planet_div.style.left = x + "px";
+
+    if (selectMesh.name === "Sun") {
+      planet_div.style.top = y - 240 + "px";
+    } else {
+      planet_div.style.top = y - 80 + "px";
+    }
+  }
+}
+
+render();
+
+function choose(event) {
+  // planet_div.src = "";
+  planet_div.innerText = "";
+  planet_div.className = "";
+  selectMesh = null;
+
+  const Sx = event.clientX;
+  const Sy = event.clientY;
+
+  const x = (Sx / window.innerWidth) * 2 - 1;
+  const y = -(Sy / window.innerHeight) * 2 + 1;
+
+  const raycaster = new THREE.Raycaster();
+  raycaster.setFromCamera(new THREE.Vector2(x, y), camera);
+
+  let intersects = raycaster.intersectObjects([sunOrib], true);
+  // let intersects = raycaster.intersectObjects(solar_system, true);
+  // console.log(intersects);
+  if (intersects.length > 0) {
+    planet_div.className = "planet_tag";
+    console.log(intersects[0].object.name);
+    planet_div.innerText = planetData[intersects[0].object.name].discription;
+    selectMesh = intersects[0].object;
+  }
+}
+
+window.addEventListener("click", choose);
